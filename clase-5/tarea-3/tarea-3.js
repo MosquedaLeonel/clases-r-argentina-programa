23925832e4dc93e44c1f0606dfe2e82eb8014c17
MosquedaLeonel <78828652+MosquedaLeonel@users.noreply.github.com>
2021-09-19 22:11:52 -0300

    agrega tarea-3
    agrega tarea=3 de la clase-5 de r/argentina-programa.

--- clase-5/tarea-3/tarea-3.js
+++ clase-5/tarea-3/tarea-3.js
@@ -6,4 +6,60 @@ cada dato.
 al apretar el botón "Calcular tiempo total", debe mostrar en un
 <strong> pre-creado el tiempo total de los videos. */ 
 
+const CANTIDAD_DE_VIDEOS = 2;
+let tiempoDeVideoTotalEnSegundos = [];
 
+const $ingresarTiempoDeVideo = document.querySelector('#ingreso-datos');
+const $calcularTiempoTotalDeVideos = document.querySelector('#calculo-tiempo-total');
+
+
+$ingresarTiempoDeVideo.onclick = function () {
+    const horasDeVideo = document.querySelector('#horas-de-video').value;
+    const minutosDeVideo = document.querySelector('#minutos-de-video').value;
+    const segundosDeVideo = document.querySelector('#segundos-de-video').value;
+    if (tiempoDeVideoTotalEnSegundos.length < CANTIDAD_DE_VIDEOS) {
+        guardarTiempo(horasDeVideo, minutosDeVideo, segundosDeVideo);
+        calculoDelTiempoEnSegundos(horasDeVideo, minutosDeVideo, segundosDeVideo);
+
+    }
+    return false;
+}
+
+
+function guardarTiempo (horasDeVideo, minutosDeVideo, segundosDeVideo) {
+    const nodoPagina = document.querySelector('form');
+    const nuevoParrafo = document.createElement('p');
+    const tiempoDeVideo = `${horasDeVideo}:${minutosDeVideo}:${segundosDeVideo}`;
+    nuevoParrafo.textContent = tiempoDeVideo;
+    nodoPagina.appendChild(nuevoParrafo);
+}
+
+function calculoDelTiempoEnSegundos (horasDeVideo, minutosDeVideo, segundosDeVideo) {
+    let horasASegundos = parseFloat(horasDeVideo * 3600);
+    let minutosASegundos = parseFloat(minutosDeVideo * 60);
+    let segundosTotalesDeVideo = parseFloat(segundosDeVideo) + minutosASegundos + horasASegundos;
+    if (tiempoDeVideoTotalEnSegundos.length < CANTIDAD_DE_VIDEOS) {
+        tiempoDeVideoTotalEnSegundos.push(segundosTotalesDeVideo);
+    }
+    
+}
+
+let resultadoEnSegundos = 0;
+
+$calcularTiempoTotalDeVideos.onclick = function () {
+    for (let i = 0; i < tiempoDeVideoTotalEnSegundos.length; i++) {
+        resultadoEnSegundos += tiempoDeVideoTotalEnSegundos[i];
+    }
+    pasajeDeSegundos(resultadoEnSegundos)
+    const nuevoCampo = document.querySelector('#campo-de-texto-resultado');
+    nuevoCampo.value = resultadoFinal;
+    return false;
+}
+let horasTotales, minutosTotales, segundosTotales;
+
+function pasajeDeSegundos (resultadoEnSegundos) {
+    horasTotales = Math.floor(resultadoEnSegundos / 3600);
+    minutosTotales = Math.floor((resultadoEnSegundos % 3600) / 60);
+    segundosTotales = Math.floor(resultadoEnSegundos % 60);
+    return let = resultadoFinal = `${horasTotales}:${minutosTotales}:${segundosTotales}`;
+}
